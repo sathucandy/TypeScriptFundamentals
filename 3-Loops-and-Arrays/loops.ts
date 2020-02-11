30341419ad9086ea1b8186d153cfbdb991844c6e
Sarthak Gupta <sarthakgupta98@gmail.com>
2020-02-11 17:51:25 +0530

    Creating loops and arrays

--- 3-Loops-and-Arrays/loops.ts
+++ 3-Loops-and-Arrays/loops.ts
@@ -1,24 +1,24 @@
 for (let index = 0; index < 5; index++) {
-    console.log(index);
+  console.log(index);
 }
 
 let total: number = 0;
-let reviews: number[] = [5,5,3,4,2];
+let reviews: number[] = [5, 5, 3, 4, 2];
 for (let i = 0; i < reviews.length; i++) {
-    console.log(reviews[i]);
-    total += reviews[i];    
+  console.log(reviews[i]);
+  total += reviews[i];
 }
-let average: number = total/reviews.length;
+let average: number = total / reviews.length;
 console.log(average);
 
 let sportsOne: string[] = ["Golf", "Cricket", "Tennis"];
 for (let i = 0; i < sportsOne.length; i++) {
-    console.log(sportsOne[i]);
+  console.log(sportsOne[i]);
 }
 
 // lets used simplified for loop
 for (let tempSport of sportsOne) {
-    console.log(tempSport);
+  console.log(tempSport);
 }
 
 // growable Arrays
@@ -27,5 +27,5 @@ sportsTwo.push("Swimming");
 sportsTwo.push("Cycling");
 
 for (let tempSport of sportsTwo) {
-    console.log(tempSport);
+  console.log(tempSport);
 }
